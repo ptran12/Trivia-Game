b209dd2ddf2f4b840d4a991bbbf4debe1cf754ee
Phi Tran <phitran@Phis-MBP.attlocal.net>
2018-11-03 17:31:11 -0500

    commit-2

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -8,23 +8,15 @@
 //need to create a count down timer & counter for correct/wrong answers
 
 
-
-//
-// alert("TEST");
-
 /////////////////////////////
 /////////////////////////////
 
 $("#start").on("click", function(){
-    // for(var i=0; i<questions.length;i++) {
-    //     $("#btnwrapper").append("<h2>"+ questions[i].question+ "</h2>");
-    //     for(var e=0;e<questions[i].answers.length;e++){
-    //         $("#btnwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[e]+"'>"+questions[i].answers[e])
-    //     }
-        
-    // }
-
     game.start();
+})
+
+$(document).on('click', '#end', function(){
+    game.done();
 })
 
 
@@ -32,35 +24,35 @@ $("#start").on("click", function(){
 var questions =[{
     question: "What is the largest planet in the solar system",
     answers: ["Saturn", "Mercury", "Jupiter", "Venus"],
-    correctAnswer:"Jupiter"
+    rightAnswers:"Jupiter"
 }, {
     question:"Which sea creature has three hearts",
     answers: ["Fish", "Shark", "Octopus", "Whale"],
-    correctAnswer: "Octopus"
+    rightAnswers: "Octopus"
 }, {
     question:"What is one quarter of 1,000",
     answers: ["200", "150", "350", "250"],
-    correctAnswer: "250"
+    rightAnswers: "250"
 }, {
     question:"Which planet shares its name with a dog?",
     answers: ["Earth", "Venus", "Pluto", "Mars"],
-    correctAnswer: "Pluto"
+    rightAnswers: "Pluto"
 }, {
     question:"When did the French Revolution End?",
     answers: ["1780", "1799", "1789", "1798"],
-    correctAnswer: "1799"
+    rightAnswers: "1799"
 }, {
     question:"What color is Absynth?",
     answers: ["Red", "Green", "Blue", "Purple"],
-    correctAnswer: "Green"
+    rightAnswers: "Green"
 }, {
     question:"In football, where do the Bronos come from?",
     answers: ["Houston", "Denver", "New York", "Cincinnati "],
-    correctAnswer: "Denver"
+    rightAnswers: "Denver"
 }, {
     question:"Topaz is the birthstone for which month?",
     answers: ["July", "November", "May", "December"],
-    correctAnswer: "November"
+    rightAnswers: "November"
 }];
 
 
@@ -82,16 +74,18 @@ var questions =[{
 //game var and timer 
 
 var game = {
-    rightAnswers: 0,
-    wrongAnswers: 0,
+    correct: 0,
+    incorrect: 0,
     counter: 120,
     timer: function(){
         game.counter--;
         $('#counter').html(game.counter);
         if(game.counter<=0){
-            alert("No more time, try again!");
+            // alert("No more time, try again!");
+            game.done();
         }
     },
+
     start: function (){
         time = setInterval(game.timer, 1000)
         $('#btnwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
@@ -101,44 +95,91 @@ var game = {
             for (var e = 0; e < questions[i].answers.length; e++) {
                 $("#btnwrapper").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[e] + "'>" + questions[i].answers[e])
             }
+        }
 
-        } 
+        $("#btnwrapper").append('<br><button id="end">DONE</button>') 
     },
 
-    check: function(){
-        $.each($("input[name='questions-1']:checked"), function (){
-            if ($(this).val() == questions[1].correctAnswer) {
-                game.rightAnswers++;
+    done: function () {
+        $.each($('input[name="question-0"]:checked'), function() {
+            if ($(this).val() === questions[0].rightAnswers) {
+                game.correct++;
             } else {
-                game.wrongAnswers++;
+                game.incorrect++;
             }
         });
 
+        $.each($('input[name="question-1"]:checked'), function () {
+            if ($(this).val() === questions[1].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
 
-        // $.each($('input[name='questions-0']:checked"), function (){
-        //     if ($(this).val() == questions[0].correctAnswer) {
-        //         game.rightAnswers++;
-        //     } else {
-        //         game.wrongAnswers++;
-        //     }
-        // });
+        $.each($('input[name="question-2"]:checked'), function () {
+            if ($(this).val() === questions[2].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
 
-        // $.each($("input[name='question - 1']:checked"), function ()){
-        //     if ($(this).val() == questions[1].correctAnswer) {
-        //         game.rightAnswers++;
-        //     } else {
-        //         game.wrongAnswers++;
-        //     }
-        // });
+        $.each($('input[name="question-3"]:checked'), function () {
+            if ($(this).val() === questions[3].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($('input[name="question-4"]:checked'), function () {
+            if ($(this).val() === questions[4].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-5"]:checked'), function () {
+            if ($(this).val() === questions[5].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-6"]:checked'), function () {
+            if ($(this).val() === questions[6].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-7"]:checked'), function () {
+            if ($(this).val() === questions[7].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-8"]:checked'), function () {
+            if ($(this).val() === questions[8].rightAnswers) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        this.result();
+    },
+
+    result: function (){
+        clearInterval(time);
+        $('#btnwrapper h2').remove();
+
+        $('#btnwrapper').html("<h2>All done!</h2>");
+        $('#btnwrapper').append("<h3>Correct Answers: " + this.correct + "</h3>");
+        $('#btnwrapper').append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+        $('#btnwrapper').append("<h3>Unanswered Questions: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
 
     }
 }
-
-
-
-
-    //print game
-    // printGame: function (){
-    //     clearInterval.(timer):
-    //     $('#subwrapper')
-    // }
